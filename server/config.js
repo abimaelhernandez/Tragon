00c255a46fdbf9e264f2e75fe08b8acbbea8542f
Bruno <bruno.torres.fabila@gmail.com>
2018-03-17 14:02:50 -0600

    Finish project boilerplate

---
+++ server/config.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes");
+const db = require('../database/index');
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/../client/dist"));
+
+//HTTP Requests go here
+
+module.exports = app;
