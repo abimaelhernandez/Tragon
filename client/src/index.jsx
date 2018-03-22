087fa7eca315f7cdced282afe4136eb9436dbaa1
JosephFernandez2689 <joe.fernandez2689@gmail.com>
2018-03-21 19:28:25 -0600

    home page for tragon

---
+++ client/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App';
+
+class App extends React.Component {
+  render(){
+    return(
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <Route path="/" component={Primarylayout} />
+        </MuiThemeProvider>
+      </BrowserRouter>
+  )}
+};
+
+ReactDOM.render(<App/>, document.getElementById('app'));
