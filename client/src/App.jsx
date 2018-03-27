fba85e6fa58d35e087b455f4c8cd0399be3ebc69
Bruno <bruno.torres.fabila@gmail.com>
2018-03-27 08:58:50 -0600

    Begin FB Auth integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,15 @@ import Mongo from './MongoTest';
 const App = () => (
   <div>
     <Mongo />
+    <div class="fb-login-button"
+      data-width="50"
+      data-max-rows="1"
+      data-size="large"
+      data-button-type="login_with"
+      data-show-faces="false"
+      data-auto-logout-link="false"
+      data-use-continue-as="false">
+    </div>
   </div>
 )
 
