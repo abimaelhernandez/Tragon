18aea4b123d57fabd9624f32fe166418f173deee
abimaelhernandez <hernandezabimael364@gmail.com>
2018-03-27 14:50:51 -0600

    elmap

--- Map/MapConfig.jsx
+++ Map/MapConfig.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 import ReactDom from "react-dom"
-import {GoogleMap, Marker} from "react-google-maps"
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps"
 
-const GoogleMap = () => {
-  
-}
+const elGoogleMap = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom = {8}
+    defaultCenter={{ lat:19.397925, lng:-99.171663}}
+  >
+    {props.isMarkerShown && <Marker position={{ lat:19.397925, lng:-99.171663 }} />}
+  </GoogleMap>
+))
+
+<elGoogleMap isMarkerShown/>
+<elGoogleMap isMarkerShown={false}/>
