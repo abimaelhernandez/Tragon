43a550f05b38f1a2bd40f1edc6f16c28c5c6fbda
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-03-29 12:21:35 -0600

    rebased

---
+++ client/src/components/home/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Search = () => (
+  <form className='SearchBar'>
+    <input
+      className='field'
+      type="text"
+      placeholder="Que se te antoja, paisa?"
+      value={''}
+    />
+</form>
+)
+
+export default Search;
