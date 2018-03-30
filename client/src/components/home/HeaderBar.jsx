6c84b1440bb458cf948b80e8c5e1baf9b0712dca
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-03-30 15:23:46 -0600

    Worked on component positioning for Landing Page *Recommit*

--- client/src/components/home/HeaderBar.jsx
+++ client/src/components/home/HeaderBar.jsx
@@ -10,11 +10,9 @@ import Toolbar from 'material-ui/Toolbar';
       <AppBar id='appBar'>
 
           <Toolbar>
-
             <Link to="/"><Button id='tabButtonHome' color="white">Home</Button></Link>
             <Link to="/user/:id"><Button id='AvatarVender' label="profile" color="white">Profile</Button></Link>
             <div className='Log'><Button id='Login' label="Userlogin" color="inherit">Log in</Button></div>
-
           </Toolbar>
       </AppBar>
   );
