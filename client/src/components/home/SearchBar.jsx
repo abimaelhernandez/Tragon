cbdbb36250ba20b06b7a3141133bbfb697226b30
abimaelhernandez <hernandezabimael364@gmail.com>
2018-04-03 13:46:43 -0500

    deleted landing page

--- client/src/components/home/SearchBar.jsx
+++ client/src/components/home/SearchBar.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import Button from 'material-ui/Button';
 
-const Searchbar = () => (
-  <form className='SearchBar'>
-    <input
-      className='field'
-      type="text"
-      placeholder="Que se te antoja, paisa?"
-      value={''}
-    />
-</form>
-)
+export default class SearchBar extends Component {
+  state = {
+    search: ''
+  }
 
-export default Searchbar;
+  updateSearch = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  newSearch = () => {
+    this.props.submitSearch(this.state.search);
+    this.setState({
+      search: ''
+    })
+  }
+
+  render() {
+
+    return (
+      <div>
+        <form className="SearchBar">
+        <input
+          onChange={this.updateSearch}
+          className='FieldInput'
+          type="text"
+          placeholder="Que se te antoja, paisa?"
+          value={this.state.search}
+          />
+      </form>
+      <Button
+        type="submit"
+        className="SearchButton"
+        variant="raised"
+        color="primary"
+        onClick={this.newSearch}>
+        Search
+      </Button>
+    </div>
+    )
+  }
+}
