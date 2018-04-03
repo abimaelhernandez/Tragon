020bcbac0b61eed85cb4bb3d6819ce2a76330d22
Bruno <bruno.torres.fabila@gmail.com>
2018-04-03 16:37:28 -0500

    Clean up info

--- client/src/components/Map/MapConfig.jsx
+++ client/src/components/Map/MapConfig.jsx
@@ -1,10 +1,11 @@
 import React from "react"
-import {compose, withProps} from "recompose"
-import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps"
+import {compose, withProps} from "recompose";
+import {GOOGLE_API} from '../../../../.env';
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps";
 
 const MyGoogleMap = compose(
   withProps({
-    googleMapURL:"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL:`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API}&callback=initMap`,
     loadingElement: <div style={{height: `75%`}} />,
     containerElement:<div style={{height: `750px`}}/>,
     mapElement:<div style={{height:`75%`}}/>,
@@ -20,7 +21,6 @@ const MyGoogleMap = compose(
     position={{lat: 19.397925, lng: -99.103199}}
     draggable={true}
     />
-     {props.isMarkerShown && <Marker position={{ lat:19.397925, lng:-99.171663 }} />}
   </GoogleMap>
 )
 
