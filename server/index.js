b8a82e154f44b391877144a9f9cc94c824d3f92a
Bruno <bruno.torres.fabila@gmail.com>
2018-04-10 16:48:55 -0500

    initialise firebase-admin for deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
-var app = require('./config.js');
+const app = require('./config.js');
+const admin = require("firebase-admin");
 
-var port = (process.env.PORT || 3000);
+const port = (process.env.PORT || 3000);
+
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: "https://tragon-8e6ce.firebaseio.com"
+});
 
 app.listen(port, function() {
-  console.log(`Listening on Port ${port}!!!`);
+  console.log(`Listening on Port ${port}`);
 });
