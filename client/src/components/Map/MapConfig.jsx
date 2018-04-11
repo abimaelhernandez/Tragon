885cf46a33ce57cf0cc602855fff76ed3bd374c7
Bruno <bruno.torres.fabila@gmail.com>
2018-04-11 16:27:35 -0500

    Remove debugging msgs for deploy

--- client/src/components/Map/MapConfig.jsx
+++ client/src/components/Map/MapConfig.jsx
@@ -4,7 +4,7 @@ import {withScriptjs, withGoogleMap, GoogleMap, Marker,Infowindow} from "react-g
 
 const MyGoogleMap = compose(
   withProps({
-    googleMapURL:`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API}&callback=initMap`,
+    googleMapURL:`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API}`,
     loadingElement: <div style={{height: `75%`}} />,
     containerElement:<div style={{height: `450px`}}/>,
     mapElement:<div style={{height:`50%`}}/>,
@@ -16,7 +16,6 @@ const MyGoogleMap = compose(
     defaultZoom = {11}
     defaultCenter={{ lat:19.397925, lng:-99.171663}}
   >
-  {console.log(props.points)}
   {props.points.map(function(obj){
     return <Marker
             position={{lat:obj.location.coordinates[0],
