37691c682a4752ac52a9d639576e97e0e6238dd5
Bruno <bruno.torres.fabila@gmail.com>
2018-04-11 15:25:43 -0500

    Edit admin initializeApp

--- server/index.js
+++ server/index.js
@@ -3,14 +3,13 @@ const admin = require("firebase-admin");
 
 const port = (process.env.PORT || 3000);
 
-
 admin.initializeApp({
   credential: admin.credential.cert({
-    "project_id": "tragon-8e6ce",
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    project_id: "tragon-8e6ce",
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
-  databaseURL: "https://tragon-8e6ce.firebaseio.com"
+  databaseURL: process.env.databaseURL
 });
 
 app.listen(port, function() {
